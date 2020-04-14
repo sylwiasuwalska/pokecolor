9a2d29b60c93da16259170e7adc99a364a645088
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-04-14 13:05:34 +0200

    refactor pagination component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,32 +1,58 @@
 import React from "react";
 import "../Pagination.css";
 
-function Pagination({rowsPerPage, totalRows, loadCurrentPage, currentPage}) {
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(totalRows / rowsPerPage); i++) {
-        pageNumbers.push(i);
+function Pagination({ rowsPerPage, totalRows, loadCurrentPage, currentPage }) {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalRows / rowsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const getFirstAndLastItem = (number, array) => {
+    if (number === 0) {
+      return 1;
     }
+    if (number === array.length+1) {
+      return array.length;
+    }
+    return number;
+  };
 
-    const getPageIndicator = (number) => {
-        return number === currentPage ? "active" : undefined;
-    };
+  return (
+    <div className="pagination">
+      <a
+        href="javascript:;"
+        onClick={() => loadCurrentPage(1)}
 
-    return (
-        <div className="pagination">
-            {" "}
-            <p>Page:</p>
-            {pageNumbers.map((number) => (
-                <a
-                    key={number}
-                    href="javascript:;"
-                    onClick={() => loadCurrentPage(number)}
-                    className={getPageIndicator(number)}
-                >
-                    {number}
-                </a>
-            ))}
-        </div>
-    );
+      >
+        first
+      </a>
+
+      <a
+        href="javascript:;"
+        onClick={() => loadCurrentPage(getFirstAndLastItem(currentPage-1, pageNumbers))}
+
+      >
+        ≪
+      </a>
+
+      <p>
+        page {currentPage} from {pageNumbers.length}
+      </p>
+
+      <a
+        href="javascript:;"
+        onClick={() => loadCurrentPage(getFirstAndLastItem(currentPage+1, pageNumbers))}
+      >
+        ≫
+      </a>
+      <a
+        href="javascript:;"
+        onClick={() => loadCurrentPage(pageNumbers.length)}
+      >
+        last
+      </a>
+    </div>
+  );
 }
 
 export default Pagination;
