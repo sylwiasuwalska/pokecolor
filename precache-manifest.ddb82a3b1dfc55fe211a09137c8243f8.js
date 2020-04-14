905924b11acd26c5957ae793649cda9b8c8ea882
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-04-14 19:20:07 +0200

    Updates

---
+++ precache-manifest.ddb82a3b1dfc55fe211a09137c8243f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651ca8b46c216d707b8b10ef043f48dd",
+    "url": "/pokecolor/index.html"
+  },
+  {
+    "revision": "6bfb604392dc756a4ce6",
+    "url": "/pokecolor/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9e07a69a5b416f74d18b",
+    "url": "/pokecolor/static/css/main.9b1cc492.chunk.css"
+  },
+  {
+    "revision": "6bfb604392dc756a4ce6",
+    "url": "/pokecolor/static/js/2.61872e2d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pokecolor/static/js/2.61872e2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e07a69a5b416f74d18b",
+    "url": "/pokecolor/static/js/main.32e0c0c8.chunk.js"
+  },
+  {
+    "revision": "d5c4767dfbfceb21dbc8",
+    "url": "/pokecolor/static/js/runtime-main.88c9c3c7.js"
+  },
+  {
+    "revision": "4c461beb017664f8c41dd25d43941c01",
+    "url": "/pokecolor/static/media/ring.4c461beb.svg"
+  }
+]);
