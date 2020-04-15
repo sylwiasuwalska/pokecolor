ff90efb09166626952c23f167c5f30fb5df209d6
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-04-15 12:22:49 +0200

    refactor sort button render

--- src/components/Sorting.js
+++ src/components/Sorting.js
@@ -1,71 +1,66 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../styles/Sorting.css";
 
-function Sorting({data, setData}) {
-    const [fieldToSort, setFieldToSort] = useState(null);
-    const [sortDirection, setSortDirection] = useState("ascending");
+function Sorting({ data, setData }) {
+  const sortingKeys = [
+    {
+      id: "name",
+      name: "name",
+    },
+    {
+      id: "baseExperience",
+      name: "base experience",
+    },
+    {
+      id: "id",
+      name: "ID",
+    },
+    {
+      id: "abilities",
+      name: "base ability",
+    },
+    {
+      id: "types",
+      name: "base type",
+    },
+  ];
 
-    const sortByField = (field) => {
-        setFieldToSort(field);
-        let sortedData = data.slice().sort((a, b) => {
-            if (a[field] < b[field]) {
-                return sortDirection === "ascending" ? -1 : 1;
-            }
-            if (a[field] > b[field]) {
-                return sortDirection === "ascending" ? 1 : -1;
-            }
-            return 0;
-        });
-        setData(sortedData);
 
-        sortDirection === "ascending"
-            ? setSortDirection("descending")
-            : setSortDirection("ascending");
-    };
+  const [fieldToSort, setFieldToSort] = useState(null);
+  const [sortDirection, setSortDirection] = useState("ascending");
 
-    const getSortIndicator = (field) => {
-        return field === fieldToSort ? `active ${sortDirection}` : undefined;
-    };
+  const sortByField = (field) => {
+    setFieldToSort(field);
+    let sortedData = data.slice().sort((a, b) => {
+      if (a[field] < b[field]) {
+        return sortDirection === "ascending" ? -1 : 1;
+      }
+      if (a[field] > b[field]) {
+        return sortDirection === "ascending" ? 1 : -1;
+      }
+      return 0;
+    });
+    setData(sortedData);
 
-    return (
-        <div className="sorting">
-            <button
-                type="button"
-                onClick={() => sortByField("name")}
-                className={getSortIndicator("name")}
-            >
-                sort by name
-            </button>
-            <button
-                type="button"
-                onClick={() => sortByField("baseExperience")}
-                className={getSortIndicator("baseExperience")}
-            >
-                sort by base experience
-            </button>
-            <button
-                type="button"
-                onClick={() => sortByField("id")}
-                className={getSortIndicator("id")}
-            >
-                sort by ID
-            </button>
-            <button
-                type="button"
-                onClick={() => sortByField("abilities")}
-                className={getSortIndicator("abilities")}
-            >
-                sort by base ability
-            </button>
-            <button
-                type="button"
-                onClick={() => sortByField("types")}
-                className={getSortIndicator("types")}
-            >
-                sort by base type
-            </button>
-        </div>
-    );
+    sortDirection === "ascending"
+      ? setSortDirection("descending")
+      : setSortDirection("ascending");
+  };
+
+  const getSortIndicator = (field) => {
+    return field === fieldToSort ? `active ${sortDirection}` : undefined;
+  };
+
+  const renderSortButtons = Object.values(sortingKeys).map((element) => {
+      return <button key={element.id} type="button" onClick={()=> sortByField(element.id)} className={getSortIndicator(element.id)}>sort by {element.name}</button>
+  })
+
+  return (
+    <div className="sorting">
+        {renderSortButtons}
+    </div>
+  );
 }
 
 export default Sorting;
+
